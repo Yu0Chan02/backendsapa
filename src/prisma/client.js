8097183ae241de9aa0aee7c3892817bf805334eb
Syafa'at Ramadhan <yuchangaming002@gmail.com>
2025-07-10 11:26:36 +0800

    deploy railway

--- src/prisma/client.js
+++ src/prisma/client.js
@@ -1,3 +1,8 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+
+prisma.$connect()
+  .then(() => console.log("✅ Berhasil konek ke database"))
+  .catch(err => console.error("❌ Gagal konek database:", err));
+
 module.exports = prisma;
